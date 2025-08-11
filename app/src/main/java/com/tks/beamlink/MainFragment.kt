1c5f55a5c63e0f3893f1bc56dc14f487c98a3a49
aaaa1597 <tokushi2011@gmail.com>
2025-08-11 20:31:35 +0900

    ActivityResultContracts.OpenMultipleDocuments()だと、フォルダ初期位置が指定できないので、ActivityResultContracts.StartActivityForResult()を使う方法に変更

--- app/src/main/java/com/tks/beamlink/MainFragment.kt
+++ app/src/main/java/com/tks/beamlink/MainFragment.kt
@@ -1,7 +1,7 @@
 package com.tks.beamlink
 
+import android.app.Activity
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.provider.DocumentsContract
 import android.provider.MediaStore
@@ -12,18 +12,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import com.tks.beamlink.databinding.FragmentMainBinding
-import androidx.core.net.toUri
 
 class MainFragment : Fragment() {
     private lateinit var _binding: FragmentMainBinding
-    private val pickMultipleDocumentsLauncher = registerForActivityResult(ActivityResultContracts.OpenMultipleDocuments()) {
-        uris: List<Uri> ->
-        // ここで選択された複数のファイルURIを処理します
-        if (uris.isNotEmpty()) {
-            Log.d("FilePicker", "Selected files count=: ${uris.size}")
-            for (uri in uris) {
+    private val pickMultipleFilesLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result ->
+        if (result.resultCode != Activity.RESULT_OK) return@registerForActivityResult
+        if(result.data==null) return@registerForActivityResult
+        if (result.data!!.clipData != null) {
+            /* 複数のファイルが選択された場合 */
+            val count = result.data!!.clipData!!.itemCount
+            for (idx in 0 until count) {
+                val uri = result.data!!.clipData!!.getItemAt(idx).uri
                 Log.d("FilePicker", "Selected file URI: $uri")
             }
+        }
+        else if (result.data!!.data != null) {
+            /* 単一のファイルが選択された場合 */
+            val uri = result.data!!.data
+            Log.d("FilePicker", "Selected file URI: $uri")
         }
     }
 
@@ -34,31 +41,28 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        _binding.btnTmp.setOnClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                /* 複数のファイル選択を許可 */
+                putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+                /* 選択可能なMIMEタイプを指定 */
+                type = "*/*"
 //        _binding.btnTmp.setOnClickListener { v -> pickMultipleDocumentsLauncher.launch(arrayOf("image/*")) }
 //        _binding.btnTmp.setOnClickListener { v -> pickMultipleDocumentsLauncher.launch(arrayOf("audio/*")) }
 //        _binding.btnTmp.setOnClickListener { v -> pickMultipleDocumentsLauncher.launch(arrayOf("video/*")) }
 //        _binding.btnTmp.setOnClickListener { v -> pickMultipleDocumentsLauncher.launch(arrayOf("text/*")) }
 //        _binding.btnTmp.setOnClickListener { v -> pickMultipleDocumentsLauncher.launch(arrayOf("application/*")) }
-        _binding.btnTmp.setOnClickListener { v -> pickMultipleDocumentsLauncher.launch(arrayOf("*/*")) }
+//        _binding.btnTmp.setOnClickListener { v -> pickMultipleDocumentsLauncher.launch(arrayOf("*/*")) }
 //        _binding.btnTmp.setOnClickListener { v -> pickMultipleDocumentsLauncher.launch(arrayOf("image/*", "audio/*", "video/*", "text/*", "application/*", "*/*")) }
+                /* EXTRA_INITIAL_URIを設定して、初期表示ディレクトリを指定 */
+                putExtra(DocumentsContract.EXTRA_INITIAL_URI, MediaStore.Downloads.EXTERNAL_CONTENT_URI)
+            }
 
-//        _binding.btnTmp.setOnClickListener { v ->
-//            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
-//                addCategory(Intent.CATEGORY_OPENABLE)
-//                // 選択可能なMIMEタイプを指定
-//                type = "*/*"
-//                putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-//
-//                // DownloadsフォルダのContent URIを指定
-//                val initialUri = MediaStore.Downloads.EXTERNAL_CONTENT_URI
-//
-//                // EXTRA_INITIAL_URIを設定
-//                putExtra(DocumentsContract.EXTRA_INITIAL_URI, initialUri)
-//            }
-//
-//            Log.d("aaaaa", "intent.type.toString()=${intent.type.toString()}")
-//            pickMultipleDocumentsLauncher.launch(arrayOf(intent.type.toString()))
-//        }
+            /* Intentを直接起動 */
+            pickMultipleFilesLauncher.launch(intent)
+        }
     }
 
     companion object {
